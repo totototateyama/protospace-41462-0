9f388190598e49b2f95a01d18b1dca51fa60c644
totototateyama <toto.tateyaman@gmail.com>
2024-09-02 19:35:15 +0900

    second commit -4

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new, :destroy]
 
   def index
     @prototypes = Prototype.all
